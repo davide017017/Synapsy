219b433dcfc0f4e33b2ec445dea55e56978be12f
Davide <davide017@hotmail.it>
2025-08-23 14:38:24 +0200

    Constrain category routes to numeric IDs

--- Backend/Modules/Categories/Routes/api.php
+++ Backend/Modules/Categories/Routes/api.php
@@ -10,12 +10,15 @@ Route::middleware(['auth:sanctum'])
         Route::get('categories', [CategoriesController::class, 'indexApi'])
             ->name('categories.index');
         Route::get('categories/{category}', [CategoriesController::class, 'showApi'])
+            ->whereNumber('category')
             ->name('categories.show');
         Route::post('categories', [CategoriesController::class, 'storeApi'])
             ->name('categories.store');
         Route::put('categories/{category}', [CategoriesController::class, 'updateApi'])
+            ->whereNumber('category')
             ->name('categories.update');
         Route::delete('categories/{category}', [CategoriesController::class, 'destroyApi'])
+            ->whereNumber('category')
             ->name('categories.destroy');
     });
 
